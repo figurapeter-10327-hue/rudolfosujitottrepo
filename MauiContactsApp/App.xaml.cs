e0d86c0cf8a3758f847fec70590a244c3f547bef
Figura Peti <figurapeter-10327@taszi.hu>
2025-12-10 23:06:56 +0100

    itt az anyag ekkor: 2025.12.10 23:06

---
+++ MauiContactsApp/App.xaml.cs
@@ -0,0 +1,18 @@
+﻿using MauiContactsApp.Data;
+namespace MauiContactsApp
+{
+    public partial class App : Application
+    {
+        public static DatabaseService Database { get; private set; } = null!;
+
+        public App()
+        {
+            InitializeComponent();
+
+            string dbPath = Path.Combine(FileSystem.AppDataDirectory, "contacts.db3");
+            Database = new DatabaseService(dbPath);
+
+            MainPage = new NavigationPage(new Views.ContactListPage());
+        }
+    }
+}
